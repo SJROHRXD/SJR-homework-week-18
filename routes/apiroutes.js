38a5a22344486706778ef5ffc4e413dfe1dc3720
SJRXD <rohr.olasz@gmail.com>
2021-10-24 19:54:41 -0700

    api, html routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,18 +1,53 @@
-const path = require("path");
+const db = require("../models");
 
 module.exports = function (app) {
-    // GET INDEX
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+    // GET WORKOUTS
+    app.get("/api/workouts", function (req, res) {
+        db.Workout.find({}).then(function (dbWorkout) {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        })
+    })
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({}).then(function (dbWorkout) {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
     })
 
-    // GET EXERCISE PAGE
-    app.get("/exercise", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    // GET A WORKOUT
+    app.get("/api/workouts/:id", function (req, res) {
+        var id = req.params.id;
+        db.Workout.findById(id, function (err, dbWorkout) {
+            if (err) {
+                console.error(err)
+            }
+            res.json(dbWorkout);
+        })
     })
 
-    // GET STATS
-    app.get("/stats", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    // POST WORKOUT
+    app.post("/api/workouts/", function (req, res) {
+        db.Workout.create({ exercise: req.body }).then(function (dbWorkout) {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        })
+    })
+
+    // UPDATE WORKOUT
+    app.put("/api/workouts/:id", function (req, res) {
+        var query = { _id: req.params.id };
+        db.Workout.findOneAndUpdate(query, {
+            $push: { exercises: [req.body] }
+        }, function (err, dbWorkout) {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(dbWorkout);
+            }
+        })
     })
 }
