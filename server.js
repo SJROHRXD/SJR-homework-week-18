0e48e6188fb75a629f5c9189cdabd9b3d3ae333d
SJRXD <rohr.olasz@gmail.com>
2021-10-24 19:56:01 -0700

    server.js, with requirements and mongo connection, will update

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
+
+app.listen(PORT, () => {
+    console.log(`Now live on port ${PORT}🏃‍♀️🏃‍♀️🏃‍♀️!`);
+});
